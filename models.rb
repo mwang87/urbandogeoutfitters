50e70c138edbb5ecc6838f3eaada492e72da97ad
ubuntu-ruby2 <mingxun@ubuntu.(none)>
2014-03-08 12:44:03 -0800

    adding model

---
+++ models.rb
@@ -0,0 +1,49 @@
+require 'dm-migrations'
+require 'dm-serializer'
+require 'dm-constraints'
+
+puts ENV
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/data.db")
+#DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_VIOLET_URL'] || 'postgres://vrsuhllnluwzby:fn7JWeN9MvlJwnQC2IKO0HOpwk@ec2-54-225-101-4.compute-1.amazonaws.com:5432/dbfgk3co86vbss' )
+
+class Picture
+    include DataMapper::Resource
+    property :id,               Serial
+    property :url,              String
+    
+    has n, :comparisons, :child_key => [ :source_id ]
+    has n, :friends, self, :through => :comparisons, :via => :target
+    
+    belongs_to :fashionableuser
+end
+
+class Comparison
+    include DataMapper::Resource
+    property :id,               Serial
+    property :comparisonvalue,  String
+    
+    belongs_to :source, 'Picture', :key => true
+    belongs_to :target, 'Picture', :key => true
+    
+    belongs_to :fashionableuser
+end
+
+
+class Fashionableuser
+    include DataMapper::Resource
+    
+    property :googleuniqueid,   String, :key => true
+    property :googleemail,      String
+    property :useralias,        String
+    
+    has n, :picture
+    has n, :comparison
+end
+
+
+DataMapper.finalize
+Picture.auto_migrate! unless Picture.storage_exists?
+Comparison.auto_migrate! unless Comparison.storage_exists?
+Fashionableuser.auto_migrate! unless Fashionableuser.storage_exists?
+DataMapper.auto_upgrade!
+
